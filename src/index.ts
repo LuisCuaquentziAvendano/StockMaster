d117e3cea24bcec34cae20f312c746b8ab53589a
Luis Antonio Cuaquentzi Avendaño <luis.cuaquentzi@gmail.com>
2024-11-04 23:03:11 -0600

    CORS added to accept requests from any domain

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { connect } from 'mongoose';
 import swaggerJSDoc from 'swagger-jsdoc';
 import { serve, setup } from 'swagger-ui-express';
+import cors from 'cors';
 import { swaggerConfig } from './swaggerConfig';
 import routes from './routes';
 import { PORT, HOST, DB_URL } from './utils/envVariables';
@@ -14,6 +15,7 @@ const app = express();
 connect(DB_URL)
 .then(() => {
     console.log('Connected to database');
+    app.use(cors());
     app.use('/api', routes);
     const swaggerDocs = swaggerJSDoc(swaggerConfig(HOST));
     app.use('/api/documentation', serve, setup(swaggerDocs));
