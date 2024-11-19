009aea37185734550d7bdb1c5158f15d0c6f2f65
carlosssfelixxx <carlosfs16@hotmail.com>
2024-11-19 10:31:41 -0700

    socket.io room listing

--- src/controllers/socket.controller.ts
+++ src/controllers/socket.controller.ts
@@ -23,6 +23,11 @@ class SocketService {
         this.io.on("connection", (socket) => {
             console.log(`new connection: ${socket.id}`);
 
+            socket.on('listRooms', () => {
+                const rooms = Array.from(this.io.sockets.adapter.rooms.keys());
+                socket.emit('roomsList', rooms);
+            });
+
             socket.on("joinRoom", (token: string) => {
                 socket.join(token);
                 console.log(`Socket ${socket.id} joined room: ${token}`);
